bb1c0de1b685e97dfe66f4a4fc283e895aa694ad
Daniel Moriya <hiromyjp@gmail.com>
2021-01-18 00:13:02 -0300

    Add project files.

---
+++ Chunker/AChunker.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Chunker
+{
+    class AChunker
+    {
+        public static IEnumerable<string> ChunkIter(string s, int chunks)
+        {
+            return DoChunk(s, chunks);
+        }
+
+        public static string[] Chunk(string s, int chunks)
+        {
+            return DoChunk(s, chunks);
+        }
+
+        private static string[] DoChunk(string s, int chunks)
+        {
+            if (chunks <= 0)
+                throw new ArgumentException("Chunks should be greater than zero");
+
+            if (string.IsNullOrEmpty(s))
+                return new string[0];
+
+            if (s.Length < chunks)
+                chunks = s.Length;
+
+            int chunksize = s.Length / chunks;
+            int diff = s.Length - chunksize * chunks;
+
+            var result = new string[chunks];
+            int sposition = 0;
+            for (int i = 0; i <= chunks - 1; i++)
+            {
+                int thisChunkSize = chunksize;
+                if (diff > 0)
+                {
+                    thisChunkSize = chunksize + 1;
+                    diff -= 1;
+                }
+
+                string substr = s.Substring(sposition, thisChunkSize);
+                result[i] = substr;
+                sposition += thisChunkSize;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Chunker/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace Chunker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var chunked = AChunker.Chunk("abcdefgh", 5);
+                foreach (var item in chunked)
+                {
+                    Console.WriteLine(item);
+                }
+
+                Console.WriteLine();
+
+                var chunked2 = AChunker.ChunkIter("abcdefgh", 10);
+                foreach (var item in chunked2)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
